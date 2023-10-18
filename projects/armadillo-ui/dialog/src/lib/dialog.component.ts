8f5b78277795a6b023da54e763607d775359fba5
Igor Rangel <igordrangel@gmail.com>
2023-10-17 21:25:42 -0300

    init

---
+++ projects/armadillo-ui/dialog/src/lib/dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-dialog',
+  template: `
+    <p>
+      dialog works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class DialogComponent {
+
+}
